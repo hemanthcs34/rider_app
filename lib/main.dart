c27668f267f38f73e9b3719fb9854b1ee20ddb65
hemanthcs34 <cshemanthcs@gmail.com>
2025-03-20 01:45:08 +0530

    Flutter project structure with platform-specific configurations and assets

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
