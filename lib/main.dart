0c4f571033be58aad7794a4876ad5a0a24c4bcc5
hemanthcs34 <cshemanthcs@gmail.com>
2025-03-20 01:45:44 +0530

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -10,3 +10,16 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
